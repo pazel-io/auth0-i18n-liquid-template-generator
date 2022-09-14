e6b530fe0484864030f452dd169bb73d63494f11
Parham <parham.fazel@spatialvision.com.au>
2022-09-14 12:13:04 +1000

    add build to git ignore

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var generateLiquidTemplate_1 = require("./generateLiquidTemplate");
-generateLiquidTemplate_1.generateEmailTemplate();
-//# sourceMappingURL=index.js.map
